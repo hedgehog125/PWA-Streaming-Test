64eda41c1ef649f44c0e2f817a430e3b4e31f3e8
Nico Clack <16543008+hedgehog125@users.noreply.github.com>
2023-10-16 23:04:42 +0100

    Fixed some of the bugs

--- src/hooks.worker.ts
+++ src/hooks.worker.ts
@@ -1,3 +1,12 @@
+/**
+ * TODO
+ * 
+ * Middle chunks are sometimes incomplete
+ * Skipping ahead and then playing through doesn't seem to cache everything
+ * 
+ * Use wrappedFetch
+ */
+
 import type { Nullable } from "sveltekit-adapter-versioned-worker/worker";
 
 import { STORAGE_PREFIX, virtualRoutes } from "sveltekit-adapter-versioned-worker/worker";
@@ -10,6 +19,9 @@ const MEDIA_STORAGE_NAME = `${STORAGE_PREFIX.slice(0, -1)}_Media`;
 
 export const handleFetch = virtualRoutes({
 	"/testVideo.mp4": async ({ request, event }) => {
+		if (request.method !== "GET") return;
+		// TODO: handle HEAD request
+
 		const originalRangeValue = request.headers.get("range");
 		if (originalRangeValue == null) return;
 
@@ -24,9 +36,15 @@ export const handleFetch = virtualRoutes({
 
 		const isExactRange = rangeStart == null || rangeEnd != null;
 		
-		const newRequest = isExactRange? request : modifyRequestHeaders(request, {
-			range: `bytes=${rangeStart}-${(rangeStart + NETWORK_CHUNK_SIZE) - 1}`
-		}, { mode: "cors" });
+		let newRequest = request;
+		if (! isExactRange) {
+			// Snap it so it ends at the end of a storage chunk
+			const newRangeEnd = (Math.ceil((rangeStart + NETWORK_CHUNK_SIZE) / STORAGE_CHUNK_SIZE) * STORAGE_CHUNK_SIZE) - 1;
+
+			newRequest = modifyRequestHeaders(request, {
+				range: `bytes=${rangeStart}-${newRangeEnd}`
+			}, { mode: "cors" });
+		} 
 
 		let res = await fetch(newRequest);
 		if (res.status === 416) { // Range not satisfiable
@@ -59,7 +77,7 @@ export const handleFetch = virtualRoutes({
 			/**
 			 * @note This is exclusive
 			 */
-			const saveToByte = isUntilEnd? rangeEnd : (saveToChunk * STORAGE_CHUNK_SIZE);
+			const saveToByte = isUntilEnd? (rangeEnd + 1) : (saveToChunk * STORAGE_CHUNK_SIZE);
 
 
 			if (saveToByte - saveFromByte <= 0) return;
@@ -73,54 +91,41 @@ export const handleFetch = virtualRoutes({
 			let networkChunk: Nullable<Uint8Array> = null;
 			let done = false;
 			while (! done) {
-				const storedURLObj = new URL("/media", location.origin);
-				storedURLObj.searchParams.set("url", request.url);
-				storedURLObj.searchParams.set("chunk", chunkID.toString());
-
 				const storedRes = new Response(new ReadableStream({
 					async start(controller) {
 						let storageChunkLength = 0;
 
+						// TODO: check on slow internet
 						while (true) {
-							//await new Promise<void>(res => setTimeout(() => res(), 10)); // TODO
-							console.log("Storage chunk", chunkID);
-
 							if (networkChunk == null) {
 								networkChunk = (await reader.read()).value?? null;
-								console.log("Read");
-								if (networkChunk == null) {
-									console.log("Done");
+								if (networkChunk == null) { // TODO: discard if stream ended earlier than expected?
 									done = true;
 									break;
 								}
 							}
-							else {
-								console.log("Reused");
-							}
 		
 							let sliceStart = Math.max((saveFromByte + excessBytesFromPrevChunk) - byteID, 0); // Prevent negatives
-							let sliceEnd = Math.max(saveToByte - byteID, 0);
+							let sliceEnd = Math.max((saveToByte + excessBytesFromPrevChunk) - byteID, 0);
 							
 							let slice = (sliceStart !== 0 || sliceEnd >= networkChunk.length)?
 								networkChunk.slice(sliceStart, sliceEnd)
 								: networkChunk
 							;
 							// TODO: avoid double slicing
-							console.log("Original slice", sliceStart + byteID, sliceEnd + byteID, "=", sliceEnd - sliceStart, slice.length);
 
 							let keepChunk = false;
 							const bytesBeforeNextStorageChunk = STORAGE_CHUNK_SIZE - storageChunkLength;
 							if (slice.length > bytesBeforeNextStorageChunk) {
 								excessBytesFromPrevChunk += bytesBeforeNextStorageChunk;
-								slice = networkChunk.slice(sliceStart, sliceStart + bytesBeforeNextStorageChunk);
+								slice = networkChunk.slice(sliceStart + excessBytesFromPrevChunk, sliceStart + excessBytesFromPrevChunk + bytesBeforeNextStorageChunk);
 								keepChunk = true;
-
-								console.log("Used changed slice", sliceStart + byteID, sliceStart + bytesBeforeNextStorageChunk + byteID, "=", bytesBeforeNextStorageChunk, slice.length);
+								console.log("Changed", chunkID, [sliceStart + excessBytesFromPrevChunk + byteID, sliceStart + excessBytesFromPrevChunk + bytesBeforeNextStorageChunk + byteID], "=", bytesBeforeNextStorageChunk, slice.length, excessBytesFromPrevChunk);
 							}
 							else {
-								console.log("Used original slice", sliceStart + byteID, sliceEnd + byteID, "=", sliceEnd - sliceStart, slice.length);
+								// TODO: this can be more than the storage chunk size
+								console.log("Original", chunkID, [sliceStart + byteID, sliceEnd + byteID], "=", sliceEnd - sliceStart, slice.length, excessBytesFromPrevChunk);
 							}
-
 
 							controller.enqueue(slice);
 							storageChunkLength += slice.length;
@@ -129,11 +134,8 @@ export const handleFetch = virtualRoutes({
 								byteID += networkChunk.length;
 								networkChunk = null;
 								excessBytesFromPrevChunk = 0;
-
-								// console.log("Kept chunk");
 							}
 							if (storageChunkLength >= STORAGE_CHUNK_SIZE) {
-								// console.log("Started next chunk");
 								// done isn't set to true
 								break;
 							}
@@ -143,47 +145,82 @@ export const handleFetch = virtualRoutes({
 					}
 				}));
 
-				await cache.put(storedURLObj, storedRes);
-				//await new Promise<void>(res => setTimeout(() => res(), 1000)); // TODO
+				await storeChunk(request.url, chunkID, storedRes, cache);
 				if (done) break;
 
 				chunkID++;
 			}
 
-
-			function getReceivedRange(res: Response): Nullable<[startInclusive: number, endInclusive: number, outOf: number]> {
-				if (res.status !== 206) {
-					const contentLengthValue = res.headers.get("content-length");
-					if (contentLengthValue == null) return null; // Responses aren't required to have a content length
-
-					const parsed = parseInt(contentLengthValue);
-					return [0, parsed - 1, parsed];
-				}
-
-				const receivedRangeValue = cloned.headers.get("content-range");
-				if (receivedRangeValue == null) return null; // Multipart byte ranges are complicated
-
-				const firstSpaceIndex = receivedRangeValue.indexOf(" ");
-				const rangeType = receivedRangeValue.slice(0, firstSpaceIndex);
-				const outerRangeValue = receivedRangeValue.slice(firstSpaceIndex + 1).trim();
-				if (rangeType !== "bytes" || outerRangeValue === "") return null;
-
-				const { 0: innerRangeValue, 1: outOfValue, length: outerRangeLength } = splitAndTrim(outerRangeValue, "/");
-				if (outerRangeLength !== 2) return null;
-
-				const innerRange = splitAndTrim(innerRangeValue, "-");
-				if (innerRange.length !== 2) return null;
-
-
-				const [rangeStart, rangeEnd, outOf] = parseStrNumArray([...innerRange, outOfValue]);
-				if (rangeStart == null || rangeEnd == null || outOf == null) return null;
-
-				return [rangeStart, rangeEnd, outOf];
-			}
+			await fetchRestOfPartialChunk(rangeStart, saveFromByte, request, cache);
 		})());
 		return res;
 	}
 });
+
+/**
+ * If there was a partial storage chunk at the start, fetch the whole thing in the background
+ */
+async function fetchRestOfPartialChunk(rangeStart: number, saveFromByte: number, request: Request, cache: Cache) {
+	if (saveFromByte !== rangeStart) {
+		const newChunkID = Math.floor(rangeStart / STORAGE_CHUNK_SIZE);
+		const newRangeStart = newChunkID * STORAGE_CHUNK_SIZE;
+		const newRangeEnd = (newRangeStart + STORAGE_CHUNK_SIZE) - 1; // Not network chunk size
+
+		const res = await fetch(request.url, {
+			// @ts-ignore
+			priority: "low",
+			headers: {
+				range: `bytes=${newRangeStart}-${newRangeEnd}`
+			},
+			mode: "cors"
+		});
+		if (res.status !== 206) return;
+		const receivedRange = getReceivedRange(res);
+		if (receivedRange == null) return;
+
+		const [receivedRangeStart, receivedRangeEnd] = receivedRange;
+		if (receivedRangeStart !== newRangeStart || receivedRangeEnd !== newRangeEnd) return;
+		
+		await storeChunk(request.url, newChunkID, new Response(res.body), cache);
+	}
+}
+function getReceivedRange(res: Response): Nullable<[startInclusive: number, endInclusive: number, outOf: number]> {
+	if (res.status !== 206) {
+		const contentLengthValue = res.headers.get("content-length");
+		if (contentLengthValue == null) return null; // Responses aren't required to have a content length
+
+		const parsed = parseInt(contentLengthValue);
+		return [0, parsed - 1, parsed];
+	}
+
+	const receivedRangeValue = res.headers.get("content-range");
+	if (receivedRangeValue == null) return null; // Multipart byte ranges are complicated
+
+	const firstSpaceIndex = receivedRangeValue.indexOf(" ");
+	const rangeType = receivedRangeValue.slice(0, firstSpaceIndex);
+	const outerRangeValue = receivedRangeValue.slice(firstSpaceIndex + 1).trim();
+	if (rangeType !== "bytes" || outerRangeValue === "") return null;
+
+	const { 0: innerRangeValue, 1: outOfValue, length: outerRangeLength } = splitAndTrim(outerRangeValue, "/");
+	if (outerRangeLength !== 2) return null;
+
+	const innerRange = splitAndTrim(innerRangeValue, "-");
+	if (innerRange.length !== 2) return null;
+
+
+	const [rangeStart, rangeEnd, outOf] = parseStrNumArray([...innerRange, outOfValue]);
+	if (rangeStart == null || rangeEnd == null || outOf == null) return null;
+
+	return [rangeStart, rangeEnd, outOf];
+}
+function storeChunk(url: string, chunkID: number, res: Response, cache: Cache): Promise<void> {
+	const storedURLObj = new URL("/media", location.origin);
+	storedURLObj.searchParams.set("url", url);
+	storedURLObj.searchParams.set("chunk", chunkID.toString());
+
+	return cache.put(storedURLObj, res);
+}
+
 
 function splitAndTrim(str: string, sep: string): string[] {
 	return str.split(sep).map(value => value.trim());
